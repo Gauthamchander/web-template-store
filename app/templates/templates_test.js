5a6e4244d33b94fb6d330e6df806cdde2dcadd45
Zaher Alyousef <zaheralyousef@gmail.com>
2016-08-23 12:04:34 +0200

    finish building the project

---
+++ app/templates/templates_test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('templateStore.templates module', function() {
+
+  beforeEach(module('templateStore.templates'));
+
+  describe('templates controller', function(){
+
+    it('should ....', inject(function($controller) {
+      //spec body
+      var templatesCtrl = $controller('TemplatesCtrl');
+      expect(templatesCtrl).toBeDefined();
+    }));
+
+  });
+});
